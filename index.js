48e419f8ebebbede9cdcc068b8a944332d5b1c30
MD Hasim <mdhasim.nana.2001@gmail.com>
2025-02-27 01:10:38 +0600

    server is ready for api create

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+// database connection link 
+const uri = process.env.URL
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // database collection 
+      const ecommerceProductCollection = client.db("ECommerceProducts").collection("products")
+
+
+    // start here server api 
+    app.get("/products", async(req, res)=>{
+        res.send(await ecommerceProductCollection.find().toArray())
+    })
+
+    app.get('/product/:id', async(req,res)=>{
+      res.send(await ecommerceProductCollection.findOne({_id: new ObjectId(req.params.id)}))
+
+    })
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res)=>{
+    res.send("E-commerce server is ready")
+})
+
+app.listen(port, ()=>{
+    console.log("e-commerce server is ready", port)
+})
